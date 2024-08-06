e87184cd72f8d17f9e07adc010226d1db1756704
Alexandr Bannykh <interind@yandex.ru>
2024-08-06 09:05:22 +0500

    task/create

---
+++ src/data/home.js
@@ -0,0 +1,24 @@
+export default {
+  lastName: 'Александр',
+  firstName: 'Банных',
+  age: 40,
+  city: 'Екатеринбург',
+  graph: 'работа удаленно',
+  work: 'Frontend developer',
+  technologies: {
+    ["react16^"]: [
+      {icon: 'hook', text: 'Hook'},
+      {icon: 'hook', text: 'ReactHookForm'},
+      {icon: 'codepen', text: 'Mui'},
+      {icon: 'codepen', text: 'Antd'}
+    ],
+    vue3: [
+      {icon: 'api', text: 'Option API'},
+      {icon: 'api', text: 'Composition API'}
+    ],
+    javaScript: [
+      {icon: 'nodejs', text: 'ES6'},
+      {icon: 'nodejs', text: 'Redux'}
+    ]
+  }
+};
